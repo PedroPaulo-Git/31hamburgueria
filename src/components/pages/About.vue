538996a052b08887abd7484776c7eaf831c8d425
PedroPaulo-Git <pedropaulopyter6@gmail.com>
2024-12-02 20:01:46 -0300

    about us to mobile almost done | going to our products

--- src/components/pages/About.vue
+++ src/components/pages/About.vue
@@ -4,31 +4,51 @@
   <section class="bg-gray-50">
     <div class="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
       <div class="mx-auto max-w-xl text-center">
-        <h1 id="h1-about" class="text-5xl font-extrabold sm:text-5xl">
-          Understand User Flow.
-          <h1 id="h1-about" class="text-5xl font-extrabold sm:text-5xl text-yellow31 sm:block"> Increase Conversion. </h1>
+
+        <div class="px-0 text-center ">
+        <h1 id="h1-about" class="text-3xl font-extrabold sm:text-5xl">
+          Não vendemos apenas hambúrgueres, 
+          <h1 id="h1-about" class="text-6xl font-extrabold sm:text-5xl text-yellow31 sm:block">
+            entregamos experiências únicas.</h1>
         </h1>
+        <img
+            src="../../assets/tracobanner.png"
+            class="w-32 mx-auto -mt-2 mb-10"
+            alt=""
+          />
+      </div>
+       
   
-        <p class="mt-4 sm:text-xl/relaxed">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo tenetur fuga ducimus
-          numquam ea!
-        </p>
-  
+
+
+
+        <div class="About mt-20 ">
+
+
+          <h1 id="h1-about" class="text-5xl my-6 font-extrabold sm:text-5xl text-black sm:block">
+            Sobre nós
+          </h1>
+          <img
+            src="../../assets/traco2.png"
+            class="w-52 h-40 mx-auto -mt-20 -mb-14"
+            alt=""
+          />
+          
+          <img src="../../assets/traco1.png" class="w-20 -rotate-12 " alt="">
+          <img class="rounded-full h-28 w-full object-cover" src="../../assets/bannerABout.jpg" alt="">
+          <p class="text-gray-600 text-sm mt-6">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod laboriosam architecto minus rerum iure nisi, magnam, asperiores corrupti vero ipsum deserunt quasi excepturi dicta quisquam? Praesentium iure saepe assumenda rem.
+          </p>
         <div class="mt-8 flex flex-wrap justify-center gap-4">
           <a
-            class="block w-full rounded bg-yellow31 px-12 py-3 text-sm font-medium text-white shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
+            class="block w-40 rounded-full bg-yellow31 px-12 py-3 text-sm font-medium text-white shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
             href="#"
           >
-            Get Started
-          </a>
-  
-          <a
-            class="block w-full rounded px-12 py-3 text-sm font-medium text-yellow31 shadow hover:text-yellow31 focus:outline-none focus:ring active:text-red-500 sm:w-auto"
-            href="#"
-          >
-            Learn More
+            Saber Mais
           </a>
         </div>
+        </div>
+     
       </div>
     </div>
   </section>
