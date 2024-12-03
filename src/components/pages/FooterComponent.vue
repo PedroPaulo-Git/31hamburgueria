263c9d0f7d2ef7b41e98064e331765e642bfc040
PedroPaulo-Git <pedropaulopyter6@gmail.com>
2024-12-02 22:38:22 -0300

    footer almost done | design

--- src/components/pages/FooterComponent.vue
+++ src/components/pages/FooterComponent.vue
@@ -1,13 +1,100 @@
 <template>
-    <div>
-        
+  <!--
+      Heads up! 👋
+    
+      Plugins:
+        - @tailwindcss/forms
+    -->
+
+  <footer class="footercomponent">
+    <div class="mx-auto max-w-screen-xl sm:px-6 lg:px-8">
+      <div class="lg:grid lg:grid-cols-2">
+        <div class="py-8 lg:py-16 lg:pe-16">
+          <div class="mt-8 grid grid-cols-1 gap-8 sm:grid-cols-3">
+            <img
+              class="w-64 -mt-10 -mb-12 mx-auto"
+              src="../../assets/logo31.png"
+              alt=""
+            />
+
+            <div class="text-center items-center flex flex-col justify-center">
+              <h1 class="text-white text-2xl font-bold">Entrar em contato</h1>
+
+              <form class="mt-4 w-full ">
+                <div
+                  class="flex mx-auto justify-between gap-2 sm:flex sm:items-center sm:gap-4 w-4/5"
+                >
+                  <input
+                    type="email"
+                    id="UserEmail"
+                    placeholder="Seu Email"
+                    class="w-full p-2 border-none sm:text-sm placeholder:p-2"
+                  />
+
+                  <button class="w-1/2 bg-yellow31">Enviar</button>
+                </div>
+              </form>
+            </div>
+          </div>
+
+          <div class="mt-8 mx-4 border-t border-gray-100 pt-8">
+            <ul class="flex flex-wrap gap-4 text-xs">
+              <li>
+                <a href="#" class="text-gray-100 transition hover:opacity-75">
+                  Terms & Conditions
+                </a>
+              </li>
+
+              <li>
+                <a href="#" class="text-gray-100 transition hover:opacity-75">
+                  Privacy Policy
+                </a>
+              </li>
+
+              <li>
+                <a href="#" class="text-gray-100 transition hover:opacity-75">
+                  Cookies
+                </a>
+              </li>
+            </ul>
+
+            <p class="mt-8 text-xs text-gray-200">
+              &copy; 2022. Company Name. All rights reserved.
+            </p>
+          </div>
+        </div>
+        <div
+          class="border-b border-gray-100 py-8 lg:order-last lg:border-b-0 lg:border-s lg:py-16 lg:ps-16 bg-yellow31"
+        >
+          <div class="block text-teal-600 lg:hidden">
+            <img
+              class="w-64 -mt-10 -mb-12 mx-auto"
+              src="../../assets/logo31.png"
+              alt=""
+            />
+          </div>
+
+          <div class="mt-8 space-y-4 lg:mt-0">
+            <span class="hidden h-1 w-10 rounded bg-teal-500 lg:block"></span>
+
+            <div>
+              <h2 class="text-2xl font-medium text-gray-900">Request a Demo</h2>
+
+              <p class="mt-4 max-w-lg text-gray-500">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Veritatis, harum deserunt nesciunt praesentium, repellendus eum
+                perspiciatis ratione pariatur a aperiam eius numquam doloribus
+                asperiores sunt.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </footer>
 </template>
 <script>
-export default {
-    
-}
+import "./styles/FooterComponent.css";
 </script>
-<style lang="">
-    
-</style>
+
+<style lang=""></style>
--- src/components/pages/OurProducts.vue
+++ src/components/pages/OurProducts.vue
@@ -1,81 +1,102 @@
 <template>
-    <div class="OurProducts ">
-      <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        <div class="items-center text-center mb-20">
-             <span id="h1-about" class="text-3xl font-extrabold sm:text-5xl">
+  <div class="OurProducts">
+    <div
+      class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8"
+    >
+      <div class="items-center text-center mb-20">
+        <span id="h1-about" class="text-3xl font-extrabold sm:text-5xl">
           Conheça Nossos
-          <h1 id="h1-about" class="text-7xl font-extrabold sm:text-5xl text-yellow31 sm:block">
-            Produtos</h1>
-          </span>
-        </div>
-       
-        <div class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 z-10 ">
-          <a v-for="product in products" :key="product.id" :href="product.href" class="group flex mx-auto relative w-64 bg-white">
+          <h1
+            id="h1-about"
+            class="text-7xl font-extrabold sm:text-5xl text-yellow31 sm:block"
+          >
+            Produtos
+          </h1>
+        </span>
+      </div>
 
-            
-            <div class="w-full flex">
-                
-                <div class="p-2 shadow-lg rounded-br-xl flex">
+      <div
+        class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 z-10"
+      >
+        <a
+          v-for="product in products"
+          :key="product.id"
+          :href="product.href"
+          class="group flex mx-auto relative w-64 bg-white"
+        >
+          <div class="w-full flex">
+            <div class="p-2 shadow-lg rounded-br-xl flex">
+              <div class="h-40 text-right">
+                <h3
+                  class="mt-4 ml-4 font-extrabold text-xl text-yellow31 text-left"
+                >
+                  {{ product.name }}
+                </h3>
 
-                    <div class="h-40 text-right">
-                    <h3 class="mt-4 ml-4 font-extrabold text-xl text-yellow31 text-left ">{{ product.name }}</h3>
-
-                    <div class="flex  h-32 ">
-                        <img :src="product.imageSrc" :alt="product.imageAlt" class="aspect-square -ml-16 -mt-2 w-40 h-40 rounded-lg object-contain xl:aspect-[7/8]" />
-                        <span class="mr-3 max-h-1/2 flex flex-col justify-between ">
-                            <p class="mt-1 text-sm font-medium text-gray-400 ">{{ product.imageAlt }}</p>
-                            <p class="mt-1 text-lg font-extrabold text-gray-700 text-end mb-4">{{ product.price }}</p>
-                        </span>           
-                    </div>
-                  
-                 </div>
+                <div class="flex h-32">
+                  <img
+                    :src="product.imageSrc"
+                    :alt="product.imageAlt"
+                    class="aspect-square -ml-16 -mt-2 w-40 h-40 rounded-lg object-contain xl:aspect-[7/8]"
+                  />
+                  <span class="mr-3 max-h-1/2 flex flex-col justify-between">
+                    <p class="mt-1 text-sm font-medium text-gray-400">
+                      {{ product.imageAlt }}
+                    </p>
+                    <p
+                      class="mt-1 text-lg font-extrabold text-gray-700 text-end mb-4"
+                    >
+                      {{ product.price }}
+                    </p>
+                  </span>
                 </div>
-                
-                
+              </div>
             </div>
-           
-          </a>
-        </div>
+          </div>
+        </a>
       </div>
     </div>
-  </template>
-  
-  <script setup>
-  import product1 from "../../assets/product1.png"
-  import './styles/OurProducts.css'
-  const products = [
-    {
-      id: 1,
-      name: 'Earthen Bottle',
-      href: '#',
-      price: 'R$48',
-      imageSrc: product1,
-      imageAlt: 'Olive drab green insulated bottle with flared screw lid and flat top.',
-    },
-    {
-      id: 2,
-      name: 'Nomad Tumbler',
-      href: '#',
-      price: 'R$35',
-      imageSrc: product1,
-      imageAlt: 'Olive drab green insulated bottle with flared screw lid and flat top.',
-    },
-    {
-      id: 3,
-      name: 'Focus Paper Refill',
-      href: '#',
-      price: 'R$89',
-      imageSrc: product1,
-      imageAlt: 'Person using a pen to cross a task off a.',
-    },
-    {
-      id: 4,
-      name: 'Machined Pencil',
-      href: '#',
-      price: 'R$35',
-      imageSrc: product1,
-      imageAlt: 'Hand holding black machined steel to cross a task off a.  ',
-    },
-    // More products...
-  ]
-  </script>
+  </div>
+</template>
+
+<script setup>
+import product1 from "../../assets/product1.png";
+import "./styles/OurProducts.css";
+const products = [
+  {
+    id: 1,
+    name: "Earthen Bottle",
+    href: "#",
+    price: "R$48",
+    imageSrc: product1,
+    imageAlt:
+      "Olive drab green insulated bottle with flared screw lid and flat top.",
+  },
+  {
+    id: 2,
+    name: "Nomad Tumbler",
+    href: "#",
+    price: "R$35",
+    imageSrc: product1,
+    imageAlt:
+      "Olive drab green insulated bottle with flared screw lid and flat top.",
+  },
+  {
+    id: 3,
+    name: "Focus Paper Refill",
+    href: "#",
+    price: "R$89",
+    imageSrc: product1,
+    imageAlt: "Person using a pen to cross a task off a.",
+  },
+  {
+    id: 4,
+    name: "Machined Pencil",
+    href: "#",
+    price: "R$35",
+    imageSrc: product1,
+    imageAlt: "Hand holding black machined steel to cross a task off a.  ",
+  },
+  // More products...
+];
+</script>
