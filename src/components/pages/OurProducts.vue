3a81b581df405a7a0632039091f95be216f3fb6e
PedroPaulo-Git <pedropaulopyter6@gmail.com>
2024-12-02 21:42:39 -0300

    OurProject.vue almost done | design

--- src/components/pages/OurProducts.vue
+++ src/components/pages/OurProducts.vue
@@ -1,20 +1,33 @@
 <template>
     <div class="bg-white">
       <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        <h2 class="sr-only">Products</h2>
-  
-        <div class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-          <a v-for="product in products" :key="product.id" :href="product.href" class="group flex relative w-full ">
+        <div class="items-center text-center mb-20">
+             <span id="h1-about" class="text-3xl font-extrabold sm:text-5xl">
+          Conheça Nossos
+          <h1 id="h1-about" class="text-7xl font-extrabold sm:text-5xl text-yellow31 sm:block">
+            Produtos</h1>
+          </span>
+        </div>
+       
+        <div class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 ">
+          <a v-for="product in products" :key="product.id" :href="product.href" class="group flex mx-auto relative w-9/12 ">
 
             
-            <div class=" w-full flex">
+            <div class="w-full flex">
                 
-                <div class="mx-2 shadow-lg rounded-br-xl flex">
-                    <img :src="product.imageSrc" :alt="product.imageAlt" class="aspect-square absolute w-40 rounded-lg object-contain group-hover:opacity-75 xl:aspect-[7/8]" />
-                    <div class="mx-10 h-40 text-right">
-                    <h3 class="mt-4 text-xl text-yellow31">{{ product.name }}</h3>
-                    <p class="mt-1 text-sm font-medium text-gray-400">{{ product.imageAlt }}</p>
-                    <p class="mt-1 text-lg font-medium text-gray-900">{{ product.price }}</p>
+                <div class="p-2 shadow-lg rounded-br-xl flex">
+
+                    <div class="h-40 text-right">
+                    <h3 class="mt-4 ml-4 font-extrabold text-xl text-yellow31 text-left ">{{ product.name }}</h3>
+
+                    <div class="flex  h-32 ">
+                        <img :src="product.imageSrc" :alt="product.imageAlt" class="aspect-square -ml-16 -mt-2 w-40 h-40 rounded-lg object-contain xl:aspect-[7/8]" />
+                        <span class="mr-3 max-h-1/2 flex flex-col justify-between ">
+                            <p class="mt-1 text-sm font-medium text-gray-400 ">{{ product.imageAlt }}</p>
+                            <p class="mt-1 text-lg font-extrabold text-gray-700 text-end mb-4">{{ product.price }}</p>
+                        </span>           
+                    </div>
+                  
                  </div>
                 </div>
                 
@@ -34,7 +47,7 @@
       id: 1,
       name: 'Earthen Bottle',
       href: '#',
-      price: '$48',
+      price: 'R$48',
       imageSrc: product1,
       imageAlt: 'Olive drab green insulated bottle with flared screw lid and flat top.',
     },
@@ -42,7 +55,7 @@
       id: 2,
       name: 'Nomad Tumbler',
       href: '#',
-      price: '$35',
+      price: 'R$35',
       imageSrc: product1,
       imageAlt: 'Olive drab green insulated bottle with flared screw lid and flat top.',
     },
@@ -50,17 +63,17 @@
       id: 3,
       name: 'Focus Paper Refill',
       href: '#',
-      price: '$89',
+      price: 'R$89',
       imageSrc: product1,
-      imageAlt: 'Person using a pen to cross a task off a productivity paper card.',
+      imageAlt: 'Person using a pen to cross a task off a.',
     },
     {
       id: 4,
-      name: 'Machined Mechanical Pencil',
+      name: 'Machined Pencil',
       href: '#',
-      price: '$35',
+      price: 'R$35',
       imageSrc: product1,
-      imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
+      imageAlt: 'Hand holding black machined steel to cross a task off a.  ',
     },
     // More products...
   ]
