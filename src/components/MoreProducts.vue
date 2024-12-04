cf2cefe10400b436f4034d6aeffee6c9e2ab475c
PedroPaulo-Git <pedropaulopyter6@gmail.com>
2024-12-03 22:00:50 -0300

    cart add and removing working

--- src/components/MoreProducts.vue
+++ src/components/MoreProducts.vue
@@ -1,61 +1,78 @@
 <template>
-    <section>
-      <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
-        <header class="text-center lg:mb-20">
-            <span id="h1-about" class="text-4xl font-extrabold sm:text-6xl ">
-                Peça outras delícias também!
-          
+  <section>
+    <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
+      <header class="text-center lg:mb-20">
+        <span id="h1-about" class="text-4xl font-extrabold sm:text-6xl">
+          Peça outras delícias também!
+
+          <div>
+            <span>ITEMS IN CART</span>
+            <div v-for="item in cart" v-bind:key="item.id">
+              <span>id:</span>{{ item.id }}
+              <span>name:</span>{{ item.name }}
+            </div>
+          </div>
         </span>
-        </header>
-    
-        <ul class="mt-8 grid gap-4 sm:grid-cols-2 lg:grid-cols-4 lg:mb-10">
-        <div v-for="food in moreFoods"
-        v-bind:key="food.id">
+      </header>
+
+      <ul class="mt-8 grid gap-4 sm:grid-cols-2 lg:grid-cols-4 lg:mb-10">
+        <div v-for="food in moreFoods" v-bind:key="food.id">
           <li>
-            <a href="#" class="group block overflow-hidden">
+            <a class="group block overflow-hidden">
               <img
-              :src="food.postImg" 
-              alt=""
+                :src="food.postImg"
+                alt=""
                 class="h-[300px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[300px]"
               />
-    
-              <div class="relative  pt-3">
+
+              <div class="relative pt-3">
                 <span class="flex justify-between items-center text-center">
-
-                    <h3 class="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4">
+                  <h3
+                    class="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4"
+                  >
                     {{ food.name }}
-                </h3>
+                  </h3>
 
-                <div class=" pr-2 flex gap-5 text-white ">
-                    <span class="bg-green-600 w-6 h-6 rounded-full flex items-center justify-center">+</span>{{ item.quantity }} <span class="bg-red-600 w-6 h-6  rounded-full flex items-center justify-center">-</span> 
-                </div>
-              
+                  <div class="flex gap-2 text-white">
+                    <button
+                      @click="btnAdd(food)"
+                      class="bg-green-600 w-6 h-6 rounded-full flex items-center justify-center"
+                    >
+                      +</button
+                    >{{ food.id }}
+                    <button
+                      @click="btnRemove(food)"
+                      class="bg-red-600 w-6 h-6 rounded-full flex items-center justify-center"
+                    >
+                      -
+                    </button>
+                  </div>
                 </span>
 
-    
                 <p class="mt-2">
-    
-                  <span class="tracking-wider text-gray-900">{{ food.price }} </span>
+                  <span class="tracking-wider text-gray-900"
+                    >{{ food.price }}
+                  </span>
                 </p>
               </div>
             </a>
           </li>
         </div>
-        </ul>
-      </div>
-    </section>
-  </template>
-  <script setup>
-import { ref } from 'vue';
-import post1 from '../assets/morefoods/insta1.jpg'
-import post2 from '../assets/morefoods/insta2.jpg'
-import post3 from '../assets/morefoods/insta3.jpg'
-import post4 from '../assets/morefoods/insta4.jpg'
-import post5 from '../assets/morefoods/insta5.jpg'
-import post6 from '../assets/morefoods/insta6.jpg'
-import post7 from '../assets/morefoods/insta7.jpg'
-import post8 from '../assets/morefoods/insta8.jpg'
-import post9 from '../assets/morefoods/insta9.jpg'
+      </ul>
+    </div>
+  </section>
+</template>
+<script setup>
+import { ref } from "vue";
+import post1 from "../assets/morefoods/insta1.jpg";
+import post2 from "../assets/morefoods/insta2.jpg";
+import post3 from "../assets/morefoods/insta3.jpg";
+import post4 from "../assets/morefoods/insta4.jpg";
+import post5 from "../assets/morefoods/insta5.jpg";
+import post6 from "../assets/morefoods/insta6.jpg";
+import post7 from "../assets/morefoods/insta7.jpg";
+import post8 from "../assets/morefoods/insta8.jpg";
+import post9 from "../assets/morefoods/insta9.jpg";
 
 const moreFoods = [
   {
@@ -108,12 +125,15 @@ const moreFoods = [
   },
 ];
 
+const cart = ref([]);
 
+const btnAdd = (food) => {
+  if (!cart.value.some((item) => item.id === food.id)) {
+    cart.value.push(food);
+  }
+};
 
-const cart= ref([]);
-
-const btnAdd = () =>{
-    
-}
-
+const btnRemove = (food) => {
+  cart.value = cart.value.filter((item) => item.id !== food.id);
+};
 </script>
